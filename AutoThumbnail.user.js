b42cc7da32f41bd22e4ff13879b3104c5ac161d9
Haruo <haruo-mtok@outlook.com>
2019-09-08 16:00:22 +0900

    start

---
+++ AutoThumbnail.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         AutoThumbnail
+// @namespace    https://github.com/haruomaki/AutoThumbnail
+// @version      0.1
+// @description  imgurの画像へのリンクを認識して、サムネイルに置き換えます。
+// @author       Haruo
+// @match        http://*.2ch.sc/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    alert('ここは2ch');
+})();
